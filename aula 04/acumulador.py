0de6c1c930f8bdf3a94feaaaf8fa6f1baf48f965
lilianpagio <lilian.pagio@gmail.com>
2026-06-04 15:35:41 -0300

    Delete aula 04/acumulador.py

--- aula 04/acumulador.py
+++
@@ -1,9 +0,0 @@
-cont = 1
-soma = 0
-
-while cont <= 5:
-    nota = float(input(f"Digite a {cont}ª nota: "))
-    soma += nota #Equivale: soma = soma + nota
-    cont += 1 #Equivale: cont = cont + 1
-media = soma / 5
-print(f"A Media Final da disciplina foi de {media}")
