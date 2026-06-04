be08f95787448a4f18030e4a52713529a59ed419
lilianpagio <lilian.pagio@gmail.com>
2026-06-04 15:35:54 -0300

    Delete aula 04/break.py

--- aula 04/break.py
+++
@@ -1,9 +0,0 @@
-print('Digite uma mensagem que irei repetir para você: ')
-print('Para encerrar escreva "sair".')
-
-while True:
-    texto = input(' ')
-    print(texto)
-    if texto == 'sair':
-        break
-print('Encerrando o programa...')
