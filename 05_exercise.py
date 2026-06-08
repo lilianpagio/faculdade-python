2936d7a06de89e03b4187d6fd524c9bdabd2ed53
lilianpagio <lilian.pagio@gmail.com>
2026-06-07 22:18:57 -0300

    Delete 05_exercise.py

--- 05_exercise.py
+++
@@ -1,9 +0,0 @@
-def border(s1):
-    size = len(s1)
-    if size:
-        print('+', '-' * size, '+')
-        print('|', s1, '|')
-        print('+', '-' * size, '+')
-
-border('Hello, world!')
-border('Progamming Logic')
